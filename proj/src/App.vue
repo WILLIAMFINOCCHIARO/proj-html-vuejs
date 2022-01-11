d9367ab01bcbc79640a6283db6d1e64d18433d3f
William Finocchiaro <89465648+WILLIAMFINOCCHIARO@users.noreply.github.com>
2022-01-11 11:24:10 +0100

    init

---
+++ proj/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <Header/>
+
+    <Main :sliderImg="imgSlider"/>
+
+    <Footer/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Main,
+    Footer,
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
