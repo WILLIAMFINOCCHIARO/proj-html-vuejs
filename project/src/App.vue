fa9226de85dabacc16e73967167a14047214cf23
William Finocchiaro <89465648+WILLIAMFINOCCHIARO@users.noreply.github.com>
2022-01-11 12:33:52 +0100

    init

---
+++ project/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <Header/>
+
+    <Main :sliderImg="imgSlider"/>
+
+    <Footer/>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+import Main from './components/Main.vue'
+import Footer from './components/Footer.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Main,
+    Footer
+  }
+}
+</script>
+
+<style lang="scss">
+@import "@/assets/commons.scss";
+@import "@/assets/ColorPhloxVar.scss";
+</style>
